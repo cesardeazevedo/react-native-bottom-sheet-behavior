d88191c1cb8ce9fe084d44cb035cbf1bb0384126
cesardeazevedo <cesardeazevedo@outlook.com>
2016-08-20 22:38:21 -0300

    Let the layout be manage by UIManager

--- android/src/main/java/com/bottomsheetbehavior/BottomSheetBehaviorView.java
+++ android/src/main/java/com/bottomsheetbehavior/BottomSheetBehaviorView.java
@@ -52,4 +52,8 @@ public class BottomSheetBehaviorView extends RelativeLayout {
             setMeasuredDimension(widthMeasureSpec, child.getHeight());
         }
     }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+    }
 }
