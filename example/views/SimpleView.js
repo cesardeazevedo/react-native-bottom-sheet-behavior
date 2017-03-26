839681a291c4d9130534830fc2838a4d1ef35756
Cesar Augusto <cesardeazevedo@outlook.com>
2017-03-26 17:28:31 -0300

    Updated SimpleView Example and added beer images

--- example/views/SimpleView.js
+++ example/views/SimpleView.js
@@ -4,9 +4,9 @@ import {
   View,
   Image,
   StatusBar,
+  ViewPagerAndroid,
   Dimensions,
   StyleSheet,
-  TouchableNativeFeedback,
 } from 'react-native'
 
 import Icon from 'react-native-vector-icons/Ionicons'
@@ -22,14 +22,6 @@ import {
 
 const { height, width } = Dimensions.get('window')
 
-const {
-  STATE_DRAGGING,
-  STATE_ANCHOR_POINT,
-  STATE_EXPANDED,
-  STATE_SETTLING,
-  STATE_COLLAPSED,
-} = AnchorSheetBehavior
-
 class SimpleView extends Component {
   static contextTypes = {
     openDrawer: PropTypes.func,
@@ -41,119 +33,70 @@ class SimpleView extends Component {
     }
   };
 
-  state = {
-    offset: 0,
-    bottomSheetState: 0,
-  };
-
-  handleState = (state) => {
-    this.bottomSheet.setBottomSheetState(state)
-  }
-
-  handleBottomSheetChange = (e) => {
-    const newState = e.nativeEvent.state
-
-    if (this.state.offset > 0.1 && (newState === STATE_DRAGGING || newState === STATE_EXPANDED)) {
-      this.setState({ bottomSheetState: 1 })
-    }
-
-    if (newState === STATE_SETTLING && !this.settlingExpanded) {
-      this.setState({ bottomSheetState: 0 })
-    }
-
-    this.lastState = newState
-  }
-
-  handleSlide = (e) => {
-    const offset = parseFloat(e.nativeEvent.offset.toFixed(2))
-
-    this.settlingExpanded = offset >= this.state.offset
-
-    if (offset === 0) {
-      this.setState({ bottomSheetState: 0 })
-    } else if (this.state.bottomSheetState !== 1 && this.lastState === STATE_DRAGGING) {
-      this.setState({ bottomSheetState: 1 })
-    }
-  }
-
   render() {
     return (
       <CoordinatorLayout style={styles.container}>
-        <StatusBar translucent backgroundColor='rgba(0, 0, 0, 0.2)' />
-        <ScrollingAppBarLayout>
+        <StatusBar translucent backgroundColor='#205cb2' />
+        <ScrollingAppBarLayout statusBarColor='#205cb2'>
           <Icon.ToolbarAndroid
             navIconName={'md-menu'}
             style={styles.toolbar}
             titleColor="#fff"
-            title="ListView"
+            title="AnchorSheet"
             onIconClicked={() => this.context.openDrawer()}
           />
         </ScrollingAppBarLayout>
         <View style={styles.content}>
-          <TouchableNativeFeedback onPress={() => this.handleState(STATE_ANCHOR_POINT)}>
-            <View style={styles.button}>
-              <Text style={styles.buttonLabel}>Anchor</Text>
-            </View>
-          </TouchableNativeFeedback>
-          <TouchableNativeFeedback onPress={() => this.handleState(STATE_EXPANDED)}>
-            <View style={styles.button}>
-              <Text style={styles.buttonLabel}>Expanded</Text>
-            </View>
-          </TouchableNativeFeedback>
-          <TouchableNativeFeedback onPress={() => this.handleState(STATE_COLLAPSED)}>
-            <View style={styles.button}>
-              <Text style={styles.buttonLabel}>Collapsed</Text>
-            </View>
-          </TouchableNativeFeedback>
         </View>
         <BackdropBottomSheet height={300}>
-          <View style={{flex: 1, backgroundColor: 'grey'}}>
-            <Image
-              resizeMode='cover'
-              style={{width, height: 300}}
-              source={require('../ReactNativelogo.jpg')}
-            />
+          <View style={{flex: 1, backgroundColor: 'white'}}>
+            <ViewPagerAndroid style={{flex: 1}}>
+              <Image
+                resizeMode="cover"
+                style={{width, height: 300}}
+                source={require('../images/beer1.jpg')}
+              />
+              <Image
+                resizeMode="cover"
+                style={{width, height: 300}}
+                source={require('../images/beer2.jpg')}
+              />
+              <Image
+                resizeMode="cover"
+                style={{width, height: 300}}
+                source={require('../images/beer3.jpg')}
+              />
+            </ViewPagerAndroid>
           </View>
         </BackdropBottomSheet>
         <AnchorSheetBehavior
           peekHeight={70}
           hideable={false}
-          ref={ref => { this.bottomSheet = ref }}
           onSlide={this.handleSlide}
           onStateChange={this.handleBottomSheetChange}>
           <View style={styles.bottomSheet}>
             <View style={styles.bottomSheetHeader}>
-              <Text style={styles.label}>BottomSheetBehavior !</Text>
-              <Text>{this.state.offset}</Text>
+              <Text style={styles.label}>AnchorSheetBehavior !</Text>
             </View>
             <View style={styles.bottomSheetContent}>
-              <TouchableNativeFeedback onPress={() => this.handleState(STATE_ANCHOR_POINT)}>
-                <View style={styles.button}>
-                  <Text style={styles.buttonLabel}>Anchor</Text>
-                </View>
-              </TouchableNativeFeedback>
-              <TouchableNativeFeedback onPress={() => this.handleState(STATE_EXPANDED)}>
-                <View style={styles.button}>
-                  <Text style={styles.buttonLabel}>Expanded</Text>
-                </View>
-              </TouchableNativeFeedback>
-              <TouchableNativeFeedback onPress={() => this.handleState(STATE_COLLAPSED)}>
-                <View style={styles.button}>
-                  <Text style={styles.buttonLabel}>Collapsed</Text>
-                </View>
-              </TouchableNativeFeedback>
             </View>
           </View>
         </AnchorSheetBehavior>
         <MergedAppBarLayout
-          mergedColor="yellow"
-          toolbarColor="yellow"
-          statusBarColor="#FFA500"
+          mergedColor="#4389f2"
+          toolbarColor="#4389f2"
+          statusBarColor="#205cb2"
           style={styles.appBarMerged}>
           <Icon.ToolbarAndroid
             navIconName="md-arrow-back"
-            titleColor="#000"
-            title="LLL"
+            overflowIconName='md-more'
+            titleColor="#fff"
+            title="AnchorSheet"
+            style={{elevation: 6}}
+            actions={[
+              {title: 'Search', show: 'always', iconName: 'md-search' },
+              {title: 'More'}
+            ]}
             onIconClicked={() => this.handleState(STATE_COLLAPSED)}
           />
         </MergedAppBarLayout>
@@ -167,7 +110,6 @@ class SimpleView extends Component {
     )
   }
 }
-
 
 const styles = StyleSheet.create({
   container: {
@@ -201,18 +143,6 @@ const styles = StyleSheet.create({
     padding: 2,
     alignItems: 'center',
     backgroundColor: '#fff',
-  },
-  button: {
-    padding: 6,
-    paddingHorizontal: 14,
-    height: 30,
-    alignSelf: 'stretch',
-    alignItems: 'center',
-    marginVertical: 1,
-    backgroundColor: '#333',
-  },
-  buttonLabel: {
-    color: '#fff'
   },
   label: {
     fontSize: 18,
