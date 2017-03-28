3c4744fdfb51eec1726e5b2cfff71851a7d4c234
Cesar Augusto <cesardeazevedo@outlook.com>
2017-03-27 22:31:32 -0300

    Added height prop on ScrollingAppBarLayout

--- android/src/main/java/com/bottomsheetbehavior/ScrollingAppBarLayoutManager.java
+++ android/src/main/java/com/bottomsheetbehavior/ScrollingAppBarLayoutManager.java
@@ -6,6 +6,7 @@ import android.os.Build;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CoordinatorLayout;
 
+import com.facebook.react.uimanager.PixelUtil;
 import com.facebook.react.uimanager.ViewGroupManager;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.annotations.ReactProp;
@@ -42,6 +43,13 @@ public class ScrollingAppBarLayoutManager extends ViewGroupManager<AppBarLayout>
     @ReactProp(name = "statusBarColor")
     public void setStatusBarColor(AppBarLayout view, String color) {
         scrollingBehavior.setStatusBarColor(Color.parseColor(color));
+    }
+
+    @ReactProp(name = "height")
+    public void setHeight(AppBarLayout view, int height) {
+        CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams) view.getLayoutParams();
+        params.height = (int) PixelUtil.toPixelFromDIP(height);
+        view.setLayoutParams(params);
     }
 
     private int getStatusBarHeight(Context context) {
